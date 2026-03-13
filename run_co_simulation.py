64672e08e1091b6da8f2415063ff574e0be8dc00
Habtemariam <h.a.kefale@tudelft.nl>
2026-03-13 11:04:29 +0100

    Clean the code

--- run_co_simulation.py
+++ run_co_simulation.py
@@ -19,18 +19,12 @@ args = parser.parse_args()
 
 configurations_folder_path = "./configurations"
 
-controller_config, settings_configs = load_configurations(
-    configurations_folder_path,
-    use_forecasted=args.use_forecasted
-)
+controller_config, settings_configs = load_configurations(configurations_folder_path, use_forecasted=args.use_forecasted)
 
 electric_grid_model = Model(electric_grid_function)
 heat_pump_model = Model(heat_pump_function)
 room_model = Model(RoomFunction(settings_configs["config 1"]))
 controller_model = Model(partial(controller_function, controller_settings=controller_config))
-
-
-
 models = [electric_grid_model, heat_pump_model, room_model, controller_model]
 manager = Manager(models, settings_configs["config 1"])
 
@@ -40,7 +34,6 @@ results = manager.run_simulation()
 # Save or compare results
 
 if not args.use_forecasted:
-
 
     with open(RESULT_FILE, "wb") as f:
         pickle.dump(results, f)
